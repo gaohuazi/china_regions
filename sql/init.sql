ef33fd87a0e85dd653f5a1f8c2ed7e18c13f5441
gaohuazi <469367264@qq.com>
2018-07-19 01:25:56 +0800

    增加 json/sql 文件

---
+++ sql/init.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS `province`;
+CREATE TABLE IF NOT EXISTS `province` (
+  `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '省份名称',
+  `province_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='省份表';
+
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE IF NOT EXISTS `city` (
+  `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '城市名称',
+  `city_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
+  `province_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
+  PRIMARY KEY (`id`),
+  KEY `province_code` (`province_code`),
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='城市表';
+
+DROP TABLE IF EXISTS `area`;
+CREATE TABLE IF NOT EXISTS `area` (
+  `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '区域名称',
+  `area_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '区域代码',
+  `city_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
+  PRIMARY KEY (`id`),
+  KEY `city_code` (`city_code`),
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='区域表';
