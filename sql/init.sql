20a7b573756b02f203ea0feaad1cda57ac43c3e3
gaohuazi <469367264@qq.com>
2018-07-20 01:23:09 +0800

    修改字段长度+合并为一张总表

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,14 @@
+--一张表
+DROP TABLE IF EXISTS `region`;
+CREATE TABLE IF NOT EXISTS `region` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pid` int(11) UNSIGNED NOT NULL  DEFAULT 0 COMMENT '父id',
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '名称',
+  PRIMARY KEY (`id`),
+  KEY `pid` (`pid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='省市区总表';
+
+-- 三张表
 DROP TABLE IF EXISTS `province`;
 CREATE TABLE IF NOT EXISTS `province` (
   `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -9,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `province` (
 DROP TABLE IF EXISTS `city`;
 CREATE TABLE IF NOT EXISTS `city` (
   `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '城市名称',
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '城市名称',
   `city_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
   `province_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
   PRIMARY KEY (`id`),
@@ -19,9 +30,10 @@ CREATE TABLE IF NOT EXISTS `city` (
 DROP TABLE IF EXISTS `area`;
 CREATE TABLE IF NOT EXISTS `area` (
   `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '区域名称',
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '区域名称',
   `area_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '区域代码',
   `city_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
   PRIMARY KEY (`id`),
   KEY `city_code` (`city_code`),
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='区域表';
+
