4b0597bde6b7daeea27ca917d1479c1a85c23054
gaohuazi <469367264@qq.com>
2021-03-06 22:30:14 +0800

    优化生成的sql语句,提升insert速度

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-2020最新省市区sql/json 文件
+2021最新省市区sql/json 文件
 
 数据来源于[国家统计局](http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/)
 
@@ -6,17 +6,17 @@
 
 有问题请提issue，持续更新中.........
 
-###目录结构描述
+### 目录结构描述
 <pre>
 ├── json                        // json文件目录
 │   ├── province.json           // 省
-│   ├── area.json               // 区
 │   ├── city.json               // 市
+│   ├── area.json               // 区
 │   └── region.json             // 包含所有省市区数据
 ├── sql                         // mysql文件目录
 │   ├── province.sql            // 省
-│   ├── area.sql                // 区
 │   ├── city.sql                // 市
+│   ├── area.sql                // 区
 │   ├── init.sql                // mysql表结构文件, 需要3张表  
 │   └── region.sql              // 包含所有省市区数据,只需1张表
 ├── LICENSE                     // MIT
--- sql/province.sql
+++ sql/province.sql
@@ -1,31 +1,32 @@
-INSERT INTO province VALUES (1, '北京市', '110000');
-INSERT INTO province VALUES (2, '天津市', '120000');
-INSERT INTO province VALUES (3, '河北省', '130000');
-INSERT INTO province VALUES (4, '山西省', '140000');
-INSERT INTO province VALUES (5, '内蒙古自治区', '150000');
-INSERT INTO province VALUES (6, '辽宁省', '210000');
-INSERT INTO province VALUES (7, '吉林省', '220000');
-INSERT INTO province VALUES (8, '黑龙江省', '230000');
-INSERT INTO province VALUES (9, '上海市', '310000');
-INSERT INTO province VALUES (10, '江苏省', '320000');
-INSERT INTO province VALUES (11, '浙江省', '330000');
-INSERT INTO province VALUES (12, '安徽省', '340000');
-INSERT INTO province VALUES (13, '福建省', '350000');
-INSERT INTO province VALUES (14, '江西省', '360000');
-INSERT INTO province VALUES (15, '山东省', '370000');
-INSERT INTO province VALUES (16, '河南省', '410000');
-INSERT INTO province VALUES (17, '湖北省', '420000');
-INSERT INTO province VALUES (18, '湖南省', '430000');
-INSERT INTO province VALUES (19, '广东省', '440000');
-INSERT INTO province VALUES (20, '广西壮族自治区', '450000');
-INSERT INTO province VALUES (21, '海南省', '460000');
-INSERT INTO province VALUES (22, '重庆市', '500000');
-INSERT INTO province VALUES (23, '四川省', '510000');
-INSERT INTO province VALUES (24, '贵州省', '520000');
-INSERT INTO province VALUES (25, '云南省', '530000');
-INSERT INTO province VALUES (26, '西藏自治区', '540000');
-INSERT INTO province VALUES (27, '陕西省', '610000');
-INSERT INTO province VALUES (28, '甘肃省', '620000');
-INSERT INTO province VALUES (29, '青海省', '630000');
-INSERT INTO province VALUES (30, '宁夏回族自治区', '640000');
-INSERT INTO province VALUES (31, '新疆维吾尔自治区', '650000');
+INSERT INTO `province` (`id`, `name`, `province_code`) VALUES
+(1, '北京市', '110000'),
+(2, '天津市', '120000'),
+(3, '河北省', '130000'),
+(4, '山西省', '140000'),
+(5, '内蒙古自治区', '150000'),
+(6, '辽宁省', '210000'),
+(7, '吉林省', '220000'),
+(8, '黑龙江省', '230000'),
+(9, '上海市', '310000'),
+(10, '江苏省', '320000'),
+(11, '浙江省', '330000'),
+(12, '安徽省', '340000'),
+(13, '福建省', '350000'),
+(14, '江西省', '360000'),
+(15, '山东省', '370000'),
+(16, '河南省', '410000'),
+(17, '湖北省', '420000'),
+(18, '湖南省', '430000'),
+(19, '广东省', '440000'),
+(20, '广西壮族自治区', '450000'),
+(21, '海南省', '460000'),
+(22, '重庆市', '500000'),
+(23, '四川省', '510000'),
+(24, '贵州省', '520000'),
+(25, '云南省', '530000'),
+(26, '西藏自治区', '540000'),
+(27, '陕西省', '610000'),
+(28, '甘肃省', '620000'),
+(29, '青海省', '630000'),
+(30, '宁夏回族自治区', '640000'),
+(31, '新疆维吾尔自治区', '650000');
