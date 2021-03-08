24471e5fb6c5e69c80ce6e8a6743fa9bc60de2ae
gaohuazi <469367264@qq.com>
2021-03-08 21:34:49 +0800

    5级联动

--- README.md
+++ README.md
@@ -6,19 +6,36 @@
 
 有问题请提issue，持续更新中.........
 
+
+
+> 分支说明：
+
+> - `master分支`：3级联动-包括省、市、区
+> - `level5分支`：5级联动-包括省、市、区、街道、社区
+
+
 ### 目录结构描述
 <pre>
 ├── json                        // json文件目录
 │   ├── province.json           // 省
 │   ├── city.json               // 市
 │   ├── area.json               // 区
+│   ├── street.json             // 街道
+│   ├── community.json          // 社区
 │   └── region.json             // 包含所有省市区数据
 ├── sql                         // mysql文件目录
 │   ├── province.sql            // 省
 │   ├── city.sql                // 市
 │   ├── area.sql                // 区
-│   ├── init.sql                // mysql表结构文件, 需要3张表  
+│   ├── street.json             // 街道
+│   ├── community.json          // 社区
+│   ├── init.sql                // mysql表结构文件, 需要5张表  
 │   └── region.sql              // 包含所有省市区数据,只需1张表
 ├── LICENSE                     // MIT
 └── Readme.md                   // help
 </pre>
+
+```mysql
+#导入sql时报错时可以尝试以下方式导入
+mysql -uroot -p --default-character-set=utf8 dbname < /path/community.sql
+```
--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,9 @@
--- 三张表
+-- 五张表
 DROP TABLE IF EXISTS `province`;
 CREATE TABLE IF NOT EXISTS `province` (
   `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL DEFAULT '' COMMENT '省份名称',
-  `province_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
+  `province_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='省份表';
 
@@ -11,8 +11,8 @@ DROP TABLE IF EXISTS `city`;
 CREATE TABLE IF NOT EXISTS `city` (
   `id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL DEFAULT '' COMMENT '城市名称',
-  `city_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
-  `province_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
+  `city_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
+  `province_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '省份代码',
   PRIMARY KEY (`id`),
   KEY `province_code` (`province_code`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='城市表';
@@ -21,9 +21,28 @@ DROP TABLE IF EXISTS `area`;
 CREATE TABLE IF NOT EXISTS `area` (
   `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL DEFAULT '' COMMENT '区域名称',
-  `area_code` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '区域代码',
-  `city_code` mediumint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
+  `area_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '区域代码',
+  `city_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '城市代码',
   PRIMARY KEY (`id`),
   KEY `city_code` (`city_code`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='区域表';
 
+DROP TABLE IF EXISTS `street`;
+CREATE TABLE IF NOT EXISTS `street` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '街道名称',
+  `street_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '街道代码',
+  `area_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '区域代码',
+  PRIMARY KEY (`id`),
+  KEY `area_code` (`area_code`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='街道表';
+
+DROP TABLE IF EXISTS `community`;
+CREATE TABLE IF NOT EXISTS `community` (
+  `id` int(7) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '社区名称',
+  `community_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '社区代码',
+  `street_code` bigint(12) UNSIGNED NOT NULL DEFAULT 0 COMMENT '街道代码',
+  PRIMARY KEY (`id`),
+  KEY `street_code` (`street_code`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='社区表';
